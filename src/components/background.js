b22c23bf417efa89abdc151c08802cd567028bcc
Ortiz25 <samueldeya@gmail.com>
2023-11-15 12:44:41 +0300

    update 4

--- src/components/background.js
+++ src/components/background.js
@@ -2,7 +2,11 @@ import React from "react";
 import classes from "./background.module.css";
 
 function Background(props) {
-  return <div className={classes.root}>{props.children}</div>;
+
+  return <div className={classes.root}>
+  {props.children}
+    
+  </div>;
 }
 
 export default Background;
